8bd71699fa295fb5b78344130f4b9391cb65c993
Carlos Daniel <ruuufffaria@gmail.com>
2022-03-29 16:27:48 -0300

    Aula 135. Componente Menu

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
+import Menu from '../template/menu'
 import Todo from '../todo/todo'
 import About from '../about/about'
 
 const App = props => (
   <div className="container">
+    <Menu />
     <Todo />
     <About />
   </div>
