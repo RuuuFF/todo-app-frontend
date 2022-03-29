25598ef12a5e88ca5e57fa7f51e9a61767e04d21
Carlos Daniel <ruuufffaria@gmail.com>
2022-03-29 16:14:28 -0300

    Aula 134. Componentes Todo e About

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
+import Todo from '../todo/todo'
+import About from '../about/about'
+
 const App = props => (
   <div className="container">
-    <h1>Teste</h1>
+    <Todo />
+    <About />
   </div>
 )
 
