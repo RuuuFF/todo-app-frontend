0907ee9c9c8aa99d4deec8af86c6873fa1043c65
Carlos Daniel <ruuufffaria@gmail.com>
2022-03-29 15:43:27 -0300

    Aula 133. Componente App

---
+++ src/main/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+const App = props => (
+  <div className="container">
+    <h1>Teste</h1>
+  </div>
+)
+
+export default App
