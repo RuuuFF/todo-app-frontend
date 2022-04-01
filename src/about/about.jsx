479ab6ef252befa10f0ab65e44b6b49342fdb1f2
Carlos Daniel <ruuufffaria@gmail.com>
2022-04-01 13:39:06 -0300

    Aula 137. Componente PageHeader

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import PageHeader from '../template/pageHeader';
+
 const About = props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='Nós' />
+
+    <h2>Nossa história</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis consequatur perferendis, delectus voluptatibus, mollitia eum facilis earum quos at explicabo repellendus! Repellendus libero, delectus voluptate nesciunt odio deserunt dignissimos? Laudantium, nesciunt reprehenderit quaerat voluptatibus enim dolor eaque deserunt eveniet tempore eos, quidem fugit non rerum accusantium. Dolorum reiciendis quibusdam maxime?</p>
+
+    <h2>Missão e visão</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, ipsam velit voluptatum, adipisci nobis ipsum, aliquam excepturi inventore libero voluptas sapiente? Repudiandae nobis ea quis voluptatum dignissimos doloremque alias. Harum animi excepturi ut quae aperiam alias qui quis voluptatum, incidunt doloribus optio soluta est asperiores reprehenderit, vel natus libero repellat earum. Dolorum quasi totam dicta id exercitationem, earum similique mollitia, ex deleniti provident a eos numquam qui et quibusdam non at nobis quod harum assumenda voluptatem cupiditate necessitatibus. Molestiae, adipisci.</p>
+
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ut nihil at corrupti perferendis id! Rem cum ut adipisci pariatur!</p>
   </div>
 )
 
