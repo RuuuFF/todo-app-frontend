08ab65cfa16155752939285856391db105c3a64c
Carlos Daniel <ruuufffaria@gmail.com>
2022-04-11 16:32:29 -0300

    Aula 150. Adicionar Teclas de Atalhos

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -3,37 +3,48 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-const TodoForm = props => (
-  <div role='form' className='todoForm'>
-    <Grid cols='12 9 10'>
-      <input
-        type="text"
-        id='description'
-        className='form-control'
-        placeholder='Adicione uma tarefa'
-        value={props.description}
-        onChange={props.handleChange}
-      />
-    </Grid>
+const TodoForm = props => {
+  const keyHandler = event => {
+    if (event.key === 'Enter') {
+      event.shiftKey ? props.handleSearch() : props.handleAdd()
+    } else if (event.key === 'Escape') {
+      props.handleClear()
+    }
+  }
 
-    <Grid cols='12 3 2'>
-      <IconButton
-        style='primary'
-        icon='plus'
-        onClick={props.handleAdd}
-      />
-      <IconButton
-        style='info'
-        icon='search'
-        onClick={props.handleSearch}
-      />
-      <IconButton
-        style='default'
-        icon='close'
-        onClick={props.handleClear}
-      />
-    </Grid>
-  </div>
-)
+  return (
+    <div role='form' className='todoForm'>
+      <Grid cols='12 9 10'>
+        <input
+          type="text"
+          id='description'
+          className='form-control'
+          placeholder='Adicione uma tarefa'
+          value={props.description}
+          onKeyUp={keyHandler}
+          onChange={props.handleChange}
+        />
+      </Grid>
+
+      <Grid cols='12 3 2'>
+        <IconButton
+          style='primary'
+          icon='plus'
+          onClick={props.handleAdd}
+        />
+        <IconButton
+          style='info'
+          icon='search'
+          onClick={props.handleSearch}
+        />
+        <IconButton
+          style='default'
+          icon='close'
+          onClick={props.handleClear}
+        />
+      </Grid>
+    </div>
+  )
+}
 
 export default TodoForm
