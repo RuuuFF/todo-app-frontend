f5497fd0a2df8ecff81d57425964f5eeafb59bd5
Carlos Daniel <ruuufffaria@gmail.com>
2022-05-04 10:11:05 -0300

    Aula 181. Aplicando Middleware (Redux-Multi)

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import promise from 'redux-promise'
+
 import reducer from './store/reducer'
+import multi from 'redux-multi'
 
 import App from './main/app'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducer, devTools)
+const store = applyMiddleware(multi, promise)(createStore)(reducer, devTools)
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/todoActions.js
+++ src/store/actions/todoActions.js
@@ -20,5 +20,8 @@ export const search = () => {
 
 export const add = description => {
   const request = axios.post(URL, { description })
-  return { type: TODO_ADDED, payload: request }
+  return [
+    { type: TODO_ADDED, payload: request },
+    search()
+  ]
 }
